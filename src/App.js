7bc9b52ce4c398b53eebdf6e13c53198236ba334
NonHelen <79176183+NonHelen@users.noreply.github.com>
2021-07-26 09:17:29 -0500

    update "coded by"

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
           <div id="weatherapp">
             <Today defaultCity= "Aguascalientes"/>
             <hr />
-            <p>
-            <a href="https://github.com/NonHelen/weather-app-react" id="coded-by" target="_blank" >coded by Helen Nonhoff</a>
+            <p id="coded-by"> coded by 
+            <a href="https://github.com/NonHelen/weather-app-react"  target="_blank" > Helen Nonhoff</a>
             </p>
           </div>
         </div>
