496cdd5295459a56855254b5244231f2b19aa802
NonHelen <79176183+NonHelen@users.noreply.github.com>
2021-08-08 15:40:57 -0500

    fix errors in Weatheforecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import ForecastDay from "./ForecastDay";
 import './WeatherForecast.css';
@@ -7,14 +7,16 @@ export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
 
+    useEffect(() => {
+    setLoaded(false);
+  }, [props.data.lat]);
+
+
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
 
-    useEffect(() => {
-    setLoaded(false);
-  }, [props.data.lat]);
 
   if (loaded) {
     return (
@@ -35,7 +37,7 @@ export default function WeatherForecast(props) {
       </div>
     );
   } else {
-    let apiURL= `https://api.openweathermap.org/data/2.5/onecall?lat=${props.data.lat}&lon=${props.coordinates.lon}&exclude={part}&appid=e622d848ff198076554702327a2f95f9&units=metric`
+    let apiURL= `https://api.openweathermap.org/data/2.5/onecall?lat=${props.data.lat}&lon=${props.data.lon}&exclude={part}&appid=e622d848ff198076554702327a2f95f9&units=metric`
     axios.get(apiURL).then(handleResponse);
 
     return null;
